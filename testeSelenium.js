82e98e6ceb3265eab4527badda04a5ac2d0aa323
Felipe de Almeida Oliveira <felipe.oliveira@SINA.SSA>
2022-02-10 16:22:06 -0300

    testes com selenium

--- testeSelenium.js
+++ testeSelenium.js
@@ -9,14 +9,14 @@ const {Builder, By, Key, until, Button} = require('selenium-webdriver');
         await driver.findElement(By.id('botaoMult')).click();
         await driver.findElement(By.id('botao1')).click();
         await driver.findElement(By.id('botao0')).click();
-        await driver.findElement(By.id('botaoCalc')).click();
+        await driver.wait(driver.findElement(By.id('botaoCalc')).click(), 1000);
         let resultado = await driver.findElement(By.id('resultado')).getText();
         console.log(resultado);
 
         await driver.get('https://www.google.com.br/');
         await driver.findElement(By.name('q')).sendKeys(resultado + " de esparta" , Key.RETURN);
         await driver.wait(until.titleIs('300 de esparta - Pesquisa Google'), 1000);
-        
+
     }finally{
         await driver.quit();
         
